bb5d620b3af8dde8b900d4b2a6236f315668f2f9
Dounia Lakhmiri <dounialakhmiri@gmail.com>
2022-10-06 11:35:33 -0400

    Update sr2_optim.py

--- sr2_optim.py
+++ sr2_optim.py
@@ -100,9 +100,14 @@ class SR2optim(Optimizer):
             # Direction with momentum
             state['vt'].mul_(self.beta).add_(1-self.beta, grad)
             flat_v = state['vt'].view(-1)
+            
+            # Adam preconditioner
+            # state['precond'].mul_(0.9).addcmul_(1 - 0.9, grad, grad)          # exponential moving average precond
+            # denom = state['precond'].sqrt() / (1 + 1e-6)   # sqrt had bias_correction 2
+            # denom.add_(group['sigma'])
 
             # Compute the step s
-            state['s'].data = self.get_step(x, state['vt'], group['sigma'], group['lmbda'])
+            state['s'].data = self.get_step(x, state['vt'], group['sigma'], group['lmbda'])         # replace sigma with denom
             norm_s += torch.sum(torch.square(state['s'])).item()
 
             # phi(x+s) ~= f(x) + grad^T * s
