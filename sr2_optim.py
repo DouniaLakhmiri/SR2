332253650759faa92811d1f09849254dd36a2e08
Dounia Lakhmiri <dounialakhmiri@gmail.com>
2022-10-06 12:34:01 -0400

    Update sr2_optim.py

--- sr2_optim.py
+++ sr2_optim.py
@@ -26,8 +26,11 @@ class SR2optim(Optimizer):
         self.failed_steps = 0
         self.stop_counter = 0
         self.beta = beta
-        
+        self.A = []
+        self.B =  []
+        self.trA2 = 0
         logging.basicConfig(level=logging.DEBUG)
+
         defaults = dict(nu1=nu1, nu2=nu2, g1=g1, g3=g3, lmbda=lmbda, sigma=sigma, weight_decay=weight_decay)
         super(SR2optim, self).__init__(params, defaults)
 
@@ -37,9 +40,11 @@ class SR2optim(Optimizer):
 
     def _copy_params(self):
         current_params = []
+        current_grads = []
         for param in self.param_groups[0]['params']:
             current_params.append(deepcopy(param.data))
-        return current_params
+            current_grads.append(deepcopy(param.grad.data))
+        return current_params, current_grads
 
     def _load_params(self, current_params):
         i = 0
@@ -52,6 +57,35 @@ class SR2optim(Optimizer):
 
     def update_weights(self, x, step, grad, sigma):
         raise NotImplementedError
+
+    def get_sTy(self):
+        sTy = 0
+        i = 0
+        for param in self.param_groups[0]['params']:
+             s = param.data - self.current_params[i].data
+             y = param.grad.data - self.current_grads[i].data
+             flat_s = s.view(-1)
+             flat_y = y.view(-1)
+             sTy += torch.dot(flat_s, flat_y).item()
+             i += 1
+        return sTy
+    
+    def initialize_A_B(self) :
+        for param in self.param_groups[0]['params']:
+            self.A.append(torch.ones_like(param.data))
+            self.B.append(torch.ones_like(param.data))
+        
+    
+    def get_denom(self, i, sigma):
+        mask = self.B[i].data > 1e-5
+        return self.B[i].data * mask.data + sigma
+
+    def update_B(self, q):
+        i = 0
+        for param in self.param_groups[0]['params']:
+            self.B[i] = torch.add(torch.add(self.B[i], -1), self.A[i], alpha=q, out=self.B[i])  
+            i += 1
+
 
     def step(self, closure=None):
         """Performs a single optimization step.
@@ -75,13 +109,18 @@ class SR2optim(Optimizer):
         l = h_x
 
         # saving the parameters in case the step is rejected
-        current_params = self._copy_params()
+        self.current_params, self.current_grads = self._copy_params()
 
         norm_s = 0
+        norm_s_sq = 0
+        sT_B_s = 0
         phi_x = f_x
         gts = 0
         stop = False
         do_updates = True
+        i = 0
+        self.trA2 = 0
+        self.initialize_A_B()
 
         for x in group['params']:
             if x.grad is None:
@@ -96,29 +135,37 @@ class SR2optim(Optimizer):
             if len(state) == 0:
                 state['s'] = torch.zeros_like(x.data)
                 state['vt'] = torch.zeros_like(grad)
-                
+                # state['precond'] = torch.zeros_like(x.data)
+                # self.initialize_A_B()
+
             # Direction with momentum
             state['vt'].mul_(self.beta).add_(1-self.beta, grad)
             flat_v = state['vt'].view(-1)
             
-            # Adam preconditioner
+            # New precond term
             # state['precond'].mul_(0.9).addcmul_(1 - 0.9, grad, grad)          # exponential moving average precond
             # denom = state['precond'].sqrt() / (1 + 1e-6)   # sqrt had bias_correction 2
             # denom.add_(group['sigma'])
 
+            denom = self.get_denom(i, group['sigma'])                            # mask(B) + sigma I 
+            
             # Compute the step s
-            state['s'].data = self.get_step(x, state['vt'], group['sigma'], group['lmbda'])         # replace sigma with denom
+            state['s'].data = self.get_step(x, state['vt'], denom, group['lmbda'])
+            self.A[i] = torch.pow(state['s'].data, 2)
+            self.trA2 += torch.sum(torch.pow(state['s'].data, 4))
             norm_s += torch.sum(torch.square(state['s'])).item()
+            norm_s_sq += norm_s ** 2
+            # sT_B_s += torch.dot(flat_s.data , torch.mul(denom.view(-1),flat_s.data)).item() 
 
             # phi(x+s) ~= f(x) + grad^T * s
             flat_g = grad.view(-1)
             flat_s = state['s'].view(-1)
             gts += torch.dot(flat_g, flat_s).item()
+            sT_B_s += torch.dot(flat_s.data , torch.mul(denom.view(-1),flat_s.data)).item()
 
             # Update the weights
-            # self.update_weights(x, state['s'], state['vt'], group['sigma'])
             x.data = x.data.add_(state['s'].data)
-
+            i += 1
 
         phi_x += gts
         # f(x+s), h(x+s)
@@ -158,6 +205,13 @@ class SR2optim(Optimizer):
                 l = hxs
                 loss.backward()
                 self.successful_steps += 1
+                # gather elements for B update
+                logging.debug('update B')
+                sT_y = self.get_sTy()
+                q = (sT_y + norm_s_sq - sT_B_s)/(self.trA2)
+                # update B := B - I + q*A
+                self.update_B(q)
+                # torch.add(torch.add(self.B, -1), q, self.A, self.B)  
             else:
                 # Reject the step
                 logging.debug('step rejected')
@@ -175,9 +229,9 @@ class SR2optiml1(SR2optim):
     def __init__(self,  *args, **kwargs):
         super().__init__( *args, **kwargs)
 
-    def get_step(self, x, grad, sigma, lmbda):
-        step = torch.max(x.data - grad / sigma - (lmbda / sigma), torch.zeros_like(x.data)) - \
-               torch.max(-x.data + grad / sigma - (lmbda / sigma), torch.zeros_like(x.data)) - x.data
+    def get_step(self, x, grad, denom, lmbda):
+        step = torch.max(x.data - grad / denom - (lmbda / denom), torch.zeros_like(x.data)) - \
+               torch.max(-x.data + grad / denom - (lmbda / denom), torch.zeros_like(x.data)) - x.data
         return step
 
     def update_weights(self, x, step, grad, sigma):
@@ -206,47 +260,3 @@ class SR2optiml0(SR2optim):
             x.data.add_(- g_over_sigma)
         return x
     
-
-class SR2optiml23(SR2optim):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def get_step(self, x, grad, sigma, lmbda):
-        g_over_sigma = grad / sigma
-        X = x.data - g_over_sigma
-        L = 2 * lmbda/sigma
-        phi = torch.arccosh(27/16 * (X**2) * (L**(-3/2)))
-        A = 2/np.sqrt(3) * L**(1/4) * (torch.cosh(phi/3)) ** (1/2)
-        cond = 2/3 * (3 * L**3)**(1/4)
-        s = ((A + ((2 * torch.abs(X))/A - A ** 2) ** (1/2)) / 2) ** 3
-
-        step = torch.where(X > cond, s - x.data,
-                           torch.where(X <  -cond, -s - x.data, -x.data))
-        return step
-
-    def update_weights(self, x, step, grad, sigma):
-        if len(x.data.shape) == 2 or len(x.data.shape) == 4:
-            x.data = x.data.add_(step.data)
-        else:
-            x.data.add_(- grad / sigma)
-        return x
-
-
-class SR2optiml12(SR2optim):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def get_step(self, x, vt, sigma, lmbda):
-        X = x.data - vt/sigma
-        p = 54**(1/3)/4 * (2 * lmbda/sigma)**(2/3)
-        a = torch.abs(X)
-        phi = torch.arccos(lmbda /(4 * sigma) * (a/3)**(-3/2))
-        s = 2/3 * a * (1 + torch.cos(2 * torch.pi /3 - 2/3 * phi ))
-
-        step = torch.where(X > p, s - x.data,
-                           torch.where(X <  -p, -s - x.data, -x.data))
-        return step
-    
-    def update_weights(self, x, step, grad, sigma):
-        x.data = x.data.add_(step.data)
-        return x
